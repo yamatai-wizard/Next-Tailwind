ab94267c1cfa0961d7a4cdedac9143104ad34f75
やまたい <yamatai0706@gmail.com>
2023-02-13 16:19:38 +0900

    link connected

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,12 +67,14 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
-        <button
-          type="button"
-          className="inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-3 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-          <PlusIconOutline className="h-6 w-6" aria-hidden="true" />
-        </button>
+        <a href="https://next-tailwind-weld.vercel.app/login">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Login
+          </button>
+        </a>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
