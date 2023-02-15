1277f6df8de28e73b598eeba7dcd923888df95ae
やまたい <yamatai0706@gmail.com>
2023-02-15 18:28:05 +0900

    added introduction page

---
+++ pages/Taiki.tsx
@@ -0,0 +1,365 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  CalendarIcon,
+  CogIcon,
+  HomeIcon,
+  MagnifyingGlassCircleIcon,
+  MapIcon,
+  MegaphoneIcon,
+  SquaresPlusIcon,
+  UserGroupIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronLeftIcon,
+  EnvelopeIcon,
+  FunnelIcon,
+  MagnifyingGlassIcon,
+  PhoneIcon,
+} from "@heroicons/react/20/solid";
+
+const user = {
+  name: "Tom Cook",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+const navigation = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: false },
+  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+  { name: "Teams", href: "#", icon: UserGroupIcon, current: false },
+  {
+    name: "Directory",
+    href: "#",
+    icon: MagnifyingGlassCircleIcon,
+    current: true,
+  },
+  { name: "Announcements", href: "#", icon: MegaphoneIcon, current: false },
+  { name: "Office Map", href: "#", icon: MapIcon, current: false },
+];
+const secondaryNavigation = [
+  { name: "Apps", href: "#", icon: SquaresPlusIcon },
+  { name: "Settings", href: "#", icon: CogIcon },
+];
+const tabs = [
+  { name: "Profile", href: "#", current: true },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Recognition", href: "#", current: false },
+];
+const profile = {
+  name: "山村大樹",
+  imageUrl:
+    "https://lh6.googleusercontent.com/proxy/eQCTbPqkJcYSI4If1-mrkBcBXlpAo0Mr5T2frQ_AAdZeTjOXiyj3G7DSsqD2CcoOkhANyL_rwRZflja7BCBAEylm2Gk3pyRw5wIzs3EnRxi6CzH9MhTsMU7KJg=w1200-h630-p-k-no-nu",
+  coverImageUrl:
+    "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+  about: `
+    <p>はじめまして！東京医科歯科大学医学部一年の山村大樹と申します。</p>
+    <p>現在は医学生として医学を学ぶ傍ら、YE Connectでプログラマーとしてのスキルを身につけるためインターンに励んでいます。将来は、医師とエンジニアのダブルフリーランス、プログラミングを利用した医療×AI関連での起業、両方の経験をもとに活躍できるコンサルなど幅広くキャリアを考えています。また、英語も得意なので4年か6年次に海外に留学し、海外で働くことも視野に入れていきたいと考えています。</p>
+  `,
+  fields: {
+    携帯番号: "秘密♡",
+    メールアドレス: "yamatai0706@gmail.com",
+    職業: "学生",
+    所属: "東京医科歯科大学医学部医学科",
+    勤務先: "MEDUCATE、ロイヤルホスト、YE Connect",
+    趣味: "ゲーム、漫画、アニメ、ドラマ、バドミントン",
+    年収: "秘密♡",
+    誕生日: "2003年7月6日",
+  },
+};
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full overflow-hidden">
+        ```
+      */}
+      <div className="flex h-full">
+        <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-40 lg:hidden"
+            onClose={setSidebarOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-40 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-white focus:outline-none">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute top-0 right-0 -mr-12 pt-2">
+                      <button
+                        type="button"
+                        className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        onClick={() => setSidebarOpen(false)}
+                      >
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
+                    <div className="flex flex-shrink-0 items-center px-4">
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=pink&shade=500"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <nav aria-label="Sidebar" className="mt-5">
+                      <div className="space-y-1 px-2">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-600 hover:bg-gray-50 hover:text-gray-900",
+                              "group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            <item.icon
+                              className={classNames(
+                                item.current
+                                  ? "text-gray-500"
+                                  : "text-gray-400 group-hover:text-gray-500",
+                                "mr-4 h-6 w-6"
+                              )}
+                              aria-hidden="true"
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                      <hr
+                        className="my-5 border-t border-gray-200"
+                        aria-hidden="true"
+                      />
+                      <div className="space-y-1 px-2">
+                        {secondaryNavigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="group flex items-center rounded-md px-2 py-2 text-base font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                          >
+                            <item.icon
+                              className="mr-4 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                              aria-hidden="true"
+                            />
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </nav>
+                  </div>
+                  <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
+                    <a href="#" className="group block flex-shrink-0">
+                      <div className="flex items-center">
+                        <div>
+                          <img
+                            className="inline-block h-10 w-10 rounded-full"
+                            src={user.imageUrl}
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-3">
+                          <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
+                            {user.name}
+                          </p>
+                          <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                            View profile
+                          </p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+              <div className="w-14 flex-shrink-0" aria-hidden="true">
+                {/* Force sidebar to shrink to fit close icon */}
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
+          <div className="lg:hidden">
+            <div className="flex items-center justify-between border-b border-gray-200 bg-gray-50 px-4 py-1.5">
+              <div>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=pink&shade=500"
+                  alt="Your Company"
+                />
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="-mr-3 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-600"
+                  onClick={() => setSidebarOpen(true)}
+                >
+                  <span className="sr-only">Open sidebar</span>
+                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="relative z-0 flex flex-1 overflow-hidden">
+            <main className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
+              <article>
+                {/* Profile header */}
+                <div>
+                  <div>
+                    <img
+                      className="h-32 w-full object-cover lg:h-48"
+                      src={profile.coverImageUrl}
+                      alt=""
+                    />
+                  </div>
+                  <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                    <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                      <div className="flex">
+                        <img
+                          className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                          src={profile.imageUrl}
+                          alt=""
+                        />
+                      </div>
+                      <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                        <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
+                          <h1 className="truncate text-2xl font-bold text-gray-900">
+                            {profile.name}
+                          </h1>
+                        </div>
+                        <div className="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                          <button
+                            type="button"
+                            className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                          >
+                            <EnvelopeIcon
+                              className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            <span>Message</span>
+                          </button>
+                          <button
+                            type="button"
+                            className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                          >
+                            <PhoneIcon
+                              className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            <span>Call</span>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
+                      <h1 className="truncate text-2xl font-bold text-gray-900">
+                        {profile.name}
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Tabs */}
+                <div className="mt-6 sm:mt-2 2xl:mt-5">
+                  <div className="border-b border-gray-200">
+                    <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                      <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                        {tabs.map((tab) => (
+                          <a
+                            key={tab.name}
+                            href={tab.href}
+                            className={classNames(
+                              tab.current
+                                ? "border-pink-500 text-gray-900"
+                                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
+                              "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                            )}
+                            aria-current={tab.current ? "page" : undefined}
+                          >
+                            {tab.name}
+                          </a>
+                        ))}
+                      </nav>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Description list */}
+                <div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+                  <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                    {Object.keys(profile.fields).map((field) => (
+                      <div key={field} className="sm:col-span-1">
+                        <dt className="text-sm font-medium text-gray-500">
+                          {field}
+                        </dt>
+                        <dd className="mt-1 text-sm text-gray-900">
+                          {profile.fields[field]}
+                        </dd>
+                      </div>
+                    ))}
+                    <div className="sm:col-span-2">
+                      <dt className="text-sm font-medium text-gray-500">
+                        About
+                      </dt>
+                      <dd
+                        className="mt-1 max-w-prose space-y-5 text-sm text-gray-900"
+                        dangerouslySetInnerHTML={{ __html: profile.about }}
+                      />
+                    </div>
+                  </dl>
+                </div>
+              </article>
+            </main>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
